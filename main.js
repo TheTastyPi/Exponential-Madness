fd4986b8c8e3e55ef70e93180906644e71b62ac7
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-05-15 17:26:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,27 +11,32 @@ setInterval(function() {
   updateStuff();
 }, 1000);
 function updateStuff() {
-  document.getElementById("number").innerHTML = game.number;
-  document.getElementById("mult1").innerHTML = game.mult.amount[1];
-  document.getElementById("multCost1").innerHTML = game.mult.cost[1];
+  document.getElementById("number").innerHTML = findDisplayValue(game.number);
+  document.getElementById("mult1").innerHTML = findDisplayValue(game.mult.amount[1]);
+  if (game.mult.unlocked[1] == false) {
+    document.getElementById("multButton1").innerHTML = "Unlock Multiplier 1 Cost: " + findDisplayValue(game.mult.cost[1]);
+  } else {
+    document.getElementById("multButton1").innerHTML = "Square Multiplier 1 Cost: " + findDisplayValue(game.mult.cost[1]);
+  }
 }
 function buyMult(n) {
-  if (game.number >= game.mult.cost[n]) {
+  if (game.number.greaterThanOrEqualTo(game.mult.cost[n])) {
     if (game.mult.unlocked[n] == false) {
       game.mult.amount[n] = game.mult.amount[n].mul(1.25);
       game.mult.unlocked[n] = true;
     } else {
       game.number = game.number.div(game.mult.cost[n]);
       game.mult.amount[n] = game.mult.amount[n].pow(2);
+      game.mult.cost[n] = game.mult.cost[n].pow(1000);
     }
+    updateStuff();
   }
-  updateStuff();
 }
 function findDisplayValue(n) {
-  if (n < 1000) {
-    return n.mag;
-  } else if (n < 1e100) {
-    return "e" + n.e;
+  if (n.lessThan(1000)) {
+    return Math.round(n*100)/100;
+  } else if (n.lessThan(Decimal.fromComponents(1, 4, 1))) {
+    return Math.round(n.m*100)/100 + "e" + n.e;
   } else {
     return "E" + n.e + "#" + n.layer;
   }
