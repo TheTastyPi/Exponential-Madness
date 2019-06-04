adcd335ec93141e9149bcc33b6d559b788a2eaaa
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-06-04 18:01:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var game = {
+  save: true,
   number: new Decimal(10),
   mult: {
     amount:[1337, new Decimal(1), new Decimal(1), new Decimal(1), new Decimal(1)],
@@ -13,6 +14,11 @@ setInterval(function() {
   game.mult.amount[3] = game.mult.amount[3].mul(game.mult.amount[4].root(100));
   updateStuff();
 }, 10);
+setInterval(function() {
+  if (autosave == true) {
+    saveGame();
+  }
+}, 60000);
 function updateStuff() {
   document.getElementById("number").innerHTML = findDisplayValue(game.number);
   document.getElementById("mult1").innerHTML = findDisplayValue(game.mult.amount[1]);
@@ -64,3 +70,79 @@ function findDisplayValue(n) {
     return "E" + n.e + "#" + n.layer;
   }
 }
+let saveName = "expmadnesssave"
+let initPlayerFunctionName = "game"
+let playerVarName = "player"
+
+function onImportError() {
+    term.echo("Error: Imported save is in invalid format, please make sure you've copied the save correctly and isn't just typing gibberish.")
+}
+
+function onLoadError() {
+    term.echo("I think you got your save messed up so bad we can't load it, the save have been exported automatically to your clipboard for debug purpose, please send it to the developer(TheTastyPi) to see what's wrong!")
+    copyStringToClipboard(save)
+}
+
+function onImportSuccess() {
+    term.echo("Save imported successfully.")
+}
+
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
+
+function saveGame() {
+  localStorage.setItem(saveName,btoa(JSON.stringify(player)))
+}
+
+function loadGame(save,imported=false) {
+  let reference = window[initPlayerFunctionName]()
+  try {
+    save = JSON.parse(atob(save))
+  } catch(err) {
+    if (imported) {
+      onImportError()
+      return
+    } else {
+      onLoadError()
+      return
+    }
+  }
+  let temp = listItems(reference)
+  let decimalList = temp[0]
+  let itemList = temp[1]
+  let missingItem = itemList.diff(listItems(save)[1])
+  if (missingItem.length != 0 && imported) {
+    if (!confirm("Your imported save seems to be missing some values, which means importing this save might be destructive, if you have made a backup of your current save and are sure about importing this save please press OK, if not, press cancel and the save will not be imported.")) {
+      return
+    }
+  }
+  missingItem.forEach(function(value) {
+    eval(`save.${value} = reference.${value}`) // No one will exploit their browser with localStorage right
+  })
+  
+  decimalList.forEach(function(value) {
+    eval(`save.${value} = new Decimal(save.${value})`)
+  })
+  
+  window[playerVarName] = save
+  if (imported) onImportSuccess()
+}
+
+function listItems(data,nestIndex="") {
+  let decimalList = []
+  let itemList = []
+  $.each(data, function (index, value) {
+    itemList.push(nestIndex + (nestIndex===""?"":".") + index)
+    if (typeof value == 'object') {
+      if (value instanceof Decimal) {
+        decimalList.push(nestIndex + (nestIndex===""?"":".") + index)
+      } else {
+        let temp = listItems(value, nestIndex + (nestIndex===""?"":".") + index)
+        decimalList = decimalList.concat(temp[0])
+        itemList = itemList.concat(temp[1])
+      }
+    }
+  });
+  return [decimalList,itemList]
+};
