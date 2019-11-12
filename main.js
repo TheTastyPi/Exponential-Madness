148faeb542ceb235e63d27e73babfd6db5071e76
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-11-12 22:02:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,36 +4,24 @@ function save() {
 
 function load() {
 	if (localStorage.getItem('emsave')) {
-		let dat = JSON.parse(localStorage.getItem('emsave'));
-		let g = {};
-		g.number = new Decimal(dat.number);
-		g.mult = {
-			amount:[0, new Decimal(dat.mult.amount[1]), new Decimal(dat.mult.amount[2]), new Decimal(dat.mult.amount[3]), new Decimal(dat.mult.amount[4])],
-			power:[0, new Decimal(dat.mult.power[1]), new Decimal(dat.mult.power[2]), new Decimal(dat.mult.power[3]), new Decimal(dat.mult.power[4])],
-			generation:[0, new Decimal(dat.mult.generation[1]), new Decimal(dat.mult.generation[2]), new Decimal(dat.mult.generation[3]), new Decimal(dat.mult.generation[4])],
-			powerPerBuy:new Decimal(dat.mult.powerPerBuy),
-			upgradeAmount:[0, new Decimal(dat.mult.upgradeAmount[1]), new Decimal(dat.mult.upgradeAmount[2]), new Decimal(dat.mult.upgradeAmount[3]), new Decimal(dat.mult.upgradeAmount[4])],
-			baseCost:[0, new Decimal(dat.mult.baseCost[1]), new Decimal(dat.mult.baseCost[2]), new Decimal(dat.mult.baseCost[3]), new Decimal(dat.mult.baseCost[4])],
-			cost:[0, new Decimal(dat.mult.cost[1]), new Decimal(dat.mult.cost[2]), new Decimal(dat.mult.cost[3]), new Decimal(dat.mult.cost[4])],
-			costIncrease:[0, new Decimal(dat.mult.costIncrease[1]), new Decimal(dat.mult.costIncrease[2]), new Decimal(dat.mult.costIncrease[3]), new Decimal(dat.mult.costIncrease[4])],
-			unlocked:[0, dat.mult.unlocked[1], dat.mult.unlocked[2], dat.mult.unlocked[3], dat.mult.unlocked[4]]
-		}
-		g.superMult = {
-			amount:[0, new Decimal(dat.superMult.amount[1]), new Decimal(dat.superMult.amount[2]), new Decimal(dat.superMult.amount[3]), new Decimal(dat.superMult.amount[4])],
-			power:[0, new Decimal(dat.superMult.power[1]), new Decimal(dat.superMult.power[2]), new Decimal(dat.superMult.power[3]), new Decimal(dat.superMult.power[4])],
-			generation:[0, new Decimal(dat.superMult.generation[1]), new Decimal(dat.superMult.generation[2]), new Decimal(dat.superMult.generation[3]), new Decimal(dat.superMult.generation[4])],
-			powerPerBuy:new Decimal(dat.superMult.powerPerBuy),
-			upgradeAmount:[0, new Decimal(dat.superMult.upgradeAmount[1]), new Decimal(dat.superMult.upgradeAmount[2]), new Decimal(dat.superMult.upgradeAmount[3]), new Decimal(dat.superMult.upgradeAmount[4])],
-			baseCost:[0, new Decimal(dat.superMult.baseCost[1]), new Decimal(dat.superMult.baseCost[2]), new Decimal(dat.superMult.baseCost[3]), new Decimal(dat.superMult.baseCost[4])],
-			cost:[0, new Decimal(dat.superMult.cost[1]), new Decimal(dat.superMult.cost[2]), new Decimal(dat.superMult.cost[3]), new Decimal(dat.superMult.cost[4])],
-			costIncrease:[0, new Decimal(dat.superMult.costIncrease[1]), new Decimal(dat.superMult.costIncrease[2]), new Decimal(dat.superMult.costIncrease[3]), new Decimal(dat.superMult.costIncrease[4])],
-			unlocked:[0, dat.superMult.unlocked[1], dat.superMult.unlocked[2], dat.superMult.unlocked[3], dat.superMult.unlocked[4]]
-		}
-		g.autoSave = dat.autoSave;
-		return g;
+		game = JSON.parse(localStorage.getItem('emsave'));
+		objectToDecimal(game);
+		return game;
 	} else {
 		return false;
 	}
+}
+
+// totally didn't copy this from somewhere else
+function objectToDecimal(object) { 
+    for(i in object) {
+        if(typeof(object[i]) == "string" && !isNaN(new Decimal(object[i]).mag)) {
+		object[i] = new Decimal(object[i]);
+	}
+        if(typeof(object[i]) == "object") {
+		objectToDecimal(object[i]);
+	}
+    }
 }
 
 function wipe() {
@@ -129,6 +117,7 @@ setInterval(function() {
   		save();
 	}
 }, 1000);
+
 function updateStuff() {
   for (let i = 1; i < game.mult.amount.length; i++) {
 	game.mult.generation[i] = game.mult.amount[i].pow(game.mult.power[i]); 
@@ -199,6 +188,7 @@ function updateStuff() {
 	  document.getElementById("autoSaveButton").innerHTML = "Auto Save: OFF";
   }
 }
+
 function buyMult(n) {
   if (game.number.greaterThanOrEqualTo(game.mult.cost[n])) {
     game.number = game.number.div(game.mult.cost[n]);
@@ -211,6 +201,7 @@ function buyMult(n) {
     updateStuff();
   }
 }
+
 function buySuperMult(n) {
   if (game.number.greaterThanOrEqualTo(game.superMult.cost[n])) {
     game.number = game.number.div(game.superMult.cost[n]);
@@ -224,6 +215,7 @@ function buySuperMult(n) {
     updateStuff();
   }
 }
+
 function findDisplay(n) {
   if (n.lessThan(1000)) {
 	return n.toFixed(2);
