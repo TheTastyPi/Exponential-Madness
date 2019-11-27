736379d42456aec4c434f7bcfb8573fb2a9dcd9b
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-11-27 17:13:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -192,18 +192,12 @@ function maxAll(type) {
 				let buyAmount = num.sub(startCost).div(increase).ceil();
 				let endCost = startCost.add(increase.mul(buyAmount));
 				let totalCost = endCost.sub(increase);
-				console.log(buyAmount);
-				console.log(totalCost);
-				console.log(num.greaterThanOrEqualTo(game.mult.cost[i]));
-				if (num.greaterThanOrEqualTo(game.mult.cost[i])) {
-					console.log("ENOUGH TO BUY");
+				if (num.greaterThanOrEqualTo(startCost)) {
 					if (game.mult.unlocked[i] == false) {
-						console.log("LOCKED");
 						game.mult.amount[i] = new Decimal(1.25);
 						game.mult.unlocked[i] = true;
 						game.number = game.number.div(game.mult.cost[i]);
 					} else {
-						console.log("UNLOCKED");
 						game.number = game.number.div((new Decimal(10)).pow((new Decimal(10)).pow(totalCost)));
 						game.mult.upgradeAmount[i] = game.mult.upgradeAmount[i].add(buyAmount);
 					}
