2376ec15163c0e7a6eadb9e3edc099b152e20e43
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-12-09 08:55:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ function nextFrame(timeStamp) {
 	let sinceLastFrame = timeStamp - lastFrame;
 	let sinceLastSave = timeStamp - lastSave;
 	if (sinceLastFrame >= game.updateSpeed) {
-		game.number = game.number.mul(game.mult.generation[1].root(1000/game.updateSpeed/game.gameSpeed));
+		game.number = game.number.mul(game.mult.generation[1].root(1000/game.updateSpeed/game.speed));
 		for (let i = 1; i < game.mult.maxMult; i++) {
-			game.mult.amount[i] = game.mult.amount[i].mul(game.mult.generation[i+1].root(1000/game.updateSpeed/game.gameSpeed));
+			game.mult.amount[i] = game.mult.amount[i].mul(game.mult.generation[i+1].root(1000/game.updateSpeed/game.speed));
 		};
-		game.mult.powerPerBuy = game.mult.powerPerBuy.mul(game.superMult.generation[1].root(1000/game.updateSpeed/game.gameSpeed))
+		game.mult.powerPerBuy = game.mult.powerPerBuy.mul(game.superMult.generation[1].root(1000/game.updateSpeed/game.speed))
 		for (let i = 1; i < game.superMult.maxMult; i++) {
-			game.superMult.amount[i] = game.superMult.amount[i].mul(game.superMult.generation[i+1].root(1000/game.updateSpeed/game.gameSpeed));
+			game.superMult.amount[i] = game.superMult.amount[i].mul(game.superMult.generation[i+1].root(1000/game.updateSpeed/game.speed));
 		};
 		updateAll();
 		lastFrame = timeStamp;
