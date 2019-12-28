dfd9ffc6d0c11b0ea7d5dbc56f628c81366a3c99
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-12-28 15:24:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -822,7 +822,8 @@ function buyMult(n) {
 
 function maxMult(n) {
 	if (!document.getElementById("mult" + n).classList.contains('hidden')) {
-		let num = game.number.log10().log10().mul(0.99999);
+		let num = game.number.log10().log10();
+		num.mag = num.mag.mul(0.999);
 		let startCost = game.mult.cost[n].log10().log10();
 		if (num.greaterThanOrEqualTo(startCost)) {
 			let increase = game.mult.costIncrease[n].log10();
@@ -873,7 +874,8 @@ function reset() {
 
 function maxReset() {
 	if (game.reset.unlocked == true) {
-		let num = game.number.log10().log10().mul(0.99999);
+		let num = game.number.log10().log10();
+		num.mag = num.mag.mul(0.999);
 		let startCost = game.reset.cost.log10().log10();
 		if (num.greaterThanOrEqualTo(startCost)) {
 			let baseCost = game.reset.baseCost.log10().log10();
@@ -924,7 +926,8 @@ function iterate() {
 
 function maxIterate() {
 	if (game.iterator.unlocked == true) {
-		let num = game.number.log10().log10().mul(0.99999);
+		let num = game.number.log10().log10();
+		num.mag = num.mag.mul(0.999);
 		let startCost = game.iterator.cost.log10().log10();
 		if (num.greaterThanOrEqualTo(startCost)) {
 			let increase = game.iterator.costIncrease.log10();
