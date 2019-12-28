cc602bfba060662c09d9fc9cc39d009e309f23c6
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-12-28 22:30:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -325,7 +325,7 @@ function toggleAutoSave() {
  ***********/
 
 function toTab(tab) {
-	document.getElementById(tab).parentNode.querySelectorAll(":scope > .tab").forEach(function(element) {
+	document.getElementById(tab).parentNode.querySelectorAll("#" + document.getElementById(tab).parentNode.id + " > .tab").forEach(function(element) {
 		element.classList.add('hidden');
 	});
 	document.getElementById(tab).classList.remove('hidden');
