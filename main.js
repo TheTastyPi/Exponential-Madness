8c575052306b33339fadce8e83525968579603e9
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2019-12-29 12:49:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -369,12 +369,12 @@ function findDisplay(n, noPoint) {
 			return n.toFixed(2);
 		}
 	} else if (n.lessThan(1e100)) {
-		return n.m.toFixed(2) + "e" + findDisplay(new Decimal(n.e, true));
+		return n.m.toFixed(2) + "e" + findDisplay(new Decimal(n.e), true);
 	} else if (n.lessThan(Decimal.fromComponents(1, 5, 1))) {
 		return "e" + findDisplay(n.log10(), true);
 	} else {
 		let x = new Decimal(n.mag).slog(10);
-		return "E" + (new Decimal(n.mag)).iteratedlog(10,x.floor()).toFixed(2) + "#" + (new Decimal(n.layer)).add(x.floor(), true);
+		return "E" + (new Decimal(n.mag)).iteratedlog(10,x.floor()).toFixed(2) + "#" + (new Decimal(n.layer)).add(x.floor());
 	}
 }
 
